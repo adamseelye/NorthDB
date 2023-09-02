54f3aa196a5b6a3f03003a7eaa5e690e37c254e2
adamseelye <aseelye@protonmail.com>
2023-09-02 12:13:55 -0600

    Code push

---
+++ src/functions.rs
@@ -0,0 +1,97 @@
+use std::io::{Write, Read, Result, Error, ErrorKind, stdin};
+use std::fs::{OpenOptions, create_dir_all};
+use std::env::current_exe;
+
+
+fn get_user_input(prompt: &str) -> Result<String> {
+    println!("{}", prompt);
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+    Ok(input.trim().to_string())
+}
+
+
+fn db_write(out: &mut dyn Write, data_in: &str) -> Result<()> {
+    let data_bytes = data_in.as_bytes();
+    out.write_all(data_bytes)?;
+    out.flush()
+}
+
+
+pub fn alter_schema(schema_in: &str, data_in: &str) -> Result<()> {
+    let get_schema = if schema_in.is_empty() {
+        get_user_input("Please enter schema name: ")?
+    } else {
+        schema_in.to_string()
+    };
+
+    let mut schema_data = if data_in.is_empty() {
+        get_user_input("Please input data: ")?
+    } else {
+        data_in.to_string()
+    };
+
+    schema_data.push('\n');
+    let db_path = format!("./db/{}.ndb", get_schema);
+
+    let mut schema_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(db_path)?;
+
+    db_write(&mut schema_file, &schema_data).expect("Failed to write to database");
+
+    Ok(())
+}
+
+
+pub fn create_schema(schema_in: &str) -> Result<()> {
+    let new_schema = if schema_in.is_empty() {
+        get_user_input("Please enter schema name: ")?
+    } else {
+        schema_in.to_string()
+    };
+
+    let current_path = current_exe()?;
+    let parent_dir = current_path.parent().ok_or(Error::new(
+            ErrorKind::Other,
+            "Failed to get parent directory",
+            ))?;
+
+    let db_dir = parent_dir.join("db");
+    println!("Attempting to create directory at: {:?}", db_dir);
+    create_dir_all(db_dir)?;
+    let db_path = format!("./db/{}.ndb", new_schema);
+
+    let mut write_schema = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(db_path)?;
+
+    db_write(&mut write_schema, "")?;
+
+    Ok(())
+}
+
+
+pub fn select_schema(schema_in: &str) -> Result<()> {
+    let get_schema = if schema_in.is_empty() {
+        get_user_input("Please enter schema name: ")?
+    } else {
+        schema_in.to_string()
+    };
+
+    let adb_path = format!("./db/{}.ndb", get_schema);
+
+    let mut schema_file = OpenOptions::new()
+        .read(true)
+        .open(adb_path)?;
+
+    let mut contents = String::new();
+    schema_file.read_to_string(&mut contents)?;
+    println!("{}", contents);
+
+    Ok(())
+}
+
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use clap::{App, Arg, SubCommand};
+
+mod functions;
+
+use crate::functions::{create_schema, alter_schema, select_schema};
+
+
+fn main() {
+
+    let matches = App::new("NorthDB")
+        .version("0.2")
+        .author("Adam Seelye")
+        .about("NorthDB")
+        .subcommand(
+            SubCommand::with_name("alter_schema")
+                .long_flag("alter")
+                .short_flag('a')
+                .about("Alter a schema")
+                .arg(
+                    Arg::with_name("schema_name")
+                    .help("Name of schema to alter")
+                    .required(true),
+                )
+                .arg(
+                    Arg::with_name("data")
+                        .short('d')
+                        .long("data")
+                        .help("Input data")
+                        .takes_value(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("create")
+                .long_flag("create")
+                .short_flag('c')
+                .about("Create a new schema")
+                .arg(
+                    Arg::with_name("schema_name")
+                        .help("Name of schema to create")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("select_schema")
+                .long_flag("select")
+                .short_flag('s')
+                .about("Select and read from a schema")
+                .arg(
+                    Arg::with_name("schema_name")
+                        .help("Name of schema to select")
+                        .required(true),
+                ),
+        )
+        .get_matches();
+
+
+    if let Some(matches) = matches.subcommand_matches("alter_schema") {
+        if let Some(schema_name) = matches.value_of("schema_name") {
+            if let Some(data) = matches.value_of("data") {
+                if let Err(e) = alter_schema(schema_name, data) {
+                    eprintln!("Error: {}", e);
+                }
+            }
+        }
+    } else if let Some(matches) = matches.subcommand_matches("create") {
+        if let Some(schema_name) = matches.value_of("schema_name") {
+            if let Err(e) = create_schema(schema_name) {
+                eprintln!("Error: {}", e);
+            }
+        }
+    } else if let Some(matches) = matches.subcommand_matches("select_schema") {
+        if let Some(schema_name) = matches.value_of("schema_name") {
+            if let Err(e) = select_schema(schema_name) {
+                eprintln!("Error: {}", e);
+            }
+        }
+    } else {
+        println!("Please specify a command. Use --help for more help.");
+    }
+}
+
